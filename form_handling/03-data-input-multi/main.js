1df18f8ff6897759658f89a98da32870bc7c66ff
Mahmoud Reda <ma7moudr3d4@gmail.com>
2024-03-16 10:13:19 +0200

    form validation

--- form_handling/03-data-input-multi/main.js
+++ form_handling/03-data-input-multi/main.js
@@ -1,17 +1,17 @@
 const InputForm = {
     template: `
         <div class="input-form">
-            <form @submit="submitForm" class="ui form">
+            <form @submit.prevent="submitForm" class="ui form">
                 <div class="field">
                     <label>New Item</label>
-                    <input v-model="fields.newItem" type="text"
-                        placeholder="Add an item!">
-                    </div>
+                    <input v-model="fields.newItem" type="text" placeholder="Add an item!">
+                    <span style="color: red;">{{ fieldErrors.newItem }}</span>
+                </div>
                 <div class="field">
                     <label>Email</label>
-                    <input v-model="fields.email" type="text"
-                        placeholder="What's your email?">
-                    </div>
+                    <input v-model="fields.email" type="text"placeholder="What's your email?">
+                    <span style="color: red;">{{ fieldErrors.email }}</span>
+                </div>
                 <div class="field">
                     <label>Urgency</label>
                     <select v-model="fields.urgency" class="ui fluid search dropdown">
@@ -20,16 +20,18 @@ const InputForm = {
                         <option>Moderate</option>
                         <option>Urgent</option>
                     </select>
+                    <span style="color: red;">{{ fieldErrors.urgency }}</span>
                 </div>
                 <div class="field">
                     <div class="ui checkbox">
                         <input v-model="fields.termsAndConditions" type="checkbox">
                         <label>I accept the terms and conditions</label>
+                        <span style="color: red;">{{ fieldErrors.termsAndConditions }}</span>
                     </div>
                 </div>
                 <button class="ui button">Submit</button>
             </form>
-            <div class="ui segment">
+            <div class="ui segment" v-if="items.length > 0">
                 <h4 class="ui header">Items</h4>
                 <ul>
                     <li v-for="item in items" class="item">{{ item }}</li>
@@ -44,15 +46,43 @@ const InputForm = {
                 urgency: '',
                 termsAndConditions: false
             },
+            fieldErrors: {
+                newItem: undefined,
+                email: undefined,
+                urgency: undefined,
+                termsAndConditions: undefined,
+            },
             items: []
         }
     },
     methods: {
         submitForm(evt) {
-            this.items.push(this.fields.newItem);
+            // evt.preventDefault();
+
+            this.fieldErrors = this.validateForm(this.fields);
+            if (Object.keys(this.fieldErrors).length) return;
+
             this.fields.newItem = '';
-            evt.preventDefault();
-        }
+            this.items.push(this.fields.newItem);
+        },
+        validateForm(fields) {
+            const errors = {};
+
+            if (!fields.newItem) errors.newItem = 'New Item required';
+            if (!fields.email) errors.email = 'Email required';
+            if (!fields.urgency) errors.urgency = 'Urgency required';
+            if (!fields.termsAndConditions) {
+                errors.termsAndConditions = 'Terms and conditions have to be approved';
+            }
+
+            if (fields.email && !this.isEmail(fields.email)) errors.email = 'Invalid Email';
+
+            return errors;
+        },
+        isEmail(email) {
+            const re = /\S+@\S+\.\S+/;
+            return re.test(email);
+        },
     }
 }
 
